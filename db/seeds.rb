0b854097785d4e9b7ee23803af471fd753453b62
Erhards Timanis <star99@inbox.lv>
2016-03-07 14:14:17 +0200

    Add case to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role_id: 1, first_name: 'Super', last_name: 'Admin')
-AdminUser.create!(email: 'darbinieks@example.com', password: 'password', password_confirmation: 'password', role_id: 2, first_name: 'Parasts', last_name: 'Mirstīgais')
-Role.create!(name: 'Admin')
-Role.create!(name: 'Darbinieks')
-Category.create!(name: 'Sūdzība')
-Company.create!(name: 'Google')
+unless AdminUser.exists?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role_id: 1, first_name: 'Super', last_name: 'Admin')
+  AdminUser.create!(email: 'darbinieks@example.com', password: 'password', password_confirmation: 'password', role_id: 2, first_name: 'Parasts', last_name: 'Mirstīgais')
+end
+unless Role.exists?
+  Role.create!(name: 'Admin')
+  Role.create!(name: 'Darbinieks')
+end
+unless Category.exists?
+  Category.create!(name: 'Sūdzība')
+end
+unless Company.exists?
+  Company.create!(name: 'Google')
+end
